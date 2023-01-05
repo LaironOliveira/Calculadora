145abecc5af660edb6e0d68b81f19a7f522f7ad9
lairon <lairon@assovio.com.br>
2023-01-05 17:58:07 -0300

    Removendo o argumento estático da propriedade calcular.

--- Calculadora/Calculadora/Frm_calc.cs
+++ Calculadora/Calculadora/Frm_calc.cs
@@ -814,7 +814,7 @@ namespace Calculadora
         }
 
         //Função responsável pelos Cálculos.
-        public static double Calcular(string Lbl_calculando, string Lbl_resultado, string operacao)
+        public double Calcular(string Lbl_calculando, string Lbl_resultado, string operacao)
         {
             double valor1;
             double valor2;
